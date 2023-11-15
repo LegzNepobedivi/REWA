cabc6ded07ca45d896842a52386da7b14cb717cf
LegzNepobedivi <139544404+LegzNepobedivi@users.noreply.github.com>
2023-11-15 23:44:49 +0100

    Add files via upload

---
+++ middleware.js
@@ -0,0 +1,65 @@
+const { stanSchema, agentSchema } = require("./schemas.js");
+
+const Stan = require("./models/stan");
+const Agent = require("./models/agent");
+
+const ExpressError = require("./utils/ExpressError");
+
+module.exports.isLoggedIn = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    req.session.returnTo = req.originalUrl; // add this line
+    req.flash("error", "You must be signed in first!");
+    return res.redirect("/login");
+  }
+  next();
+};
+
+module.exports.storeReturnTo = (req, res, next) => {
+  if (req.session.returnTo) {
+    res.locals.returnTo = req.session.returnTo;
+  }
+  next();
+};
+
+module.exports.validateStan = (req, res, next) => {
+  const { error } = stanSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+module.exports.isAuthor = async (req, res, next) => {
+  const { id } = req.params;
+  const stan = await Stan.findById(id);
+  if (!stan.author.equals(req.user._id)) {
+    req.flash("error", "Not your stan");
+    return res.redirect(`/stanovi/${id}`);
+  }
+  next();
+};
+
+module.exports.isAgentAuthor = async (req, res, next) => {
+  const { agentId } = req.params;
+  const agent = await Agent.findById(agentId);
+  if (!agent.author.equals(req.user._id)) {
+    req.flash("error", "Not your stan");
+    return res.redirect(`/agenti`);
+  }
+  next();
+};
+
+module.exports.validateAgent = (req, res, next) => {
+  console.log(req.body);
+  const { error } = agentSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+// module.exports.isLoggedIn;
